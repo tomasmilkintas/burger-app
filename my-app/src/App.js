673e970ea09376553d38f88d69d21d14586f910d
Tomas <tomas.milkintas1@gmail.com>
2020-06-24 14:35:40 +0100

    displaying user specific orders

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Layout from "./hoc/Layout/Layout";
@@ -12,30 +12,52 @@ import * as actions from "./store/actions/index";
 
 class App extends Component {
     componentDidMount() {
-        this.props.onTryAutoSingup();
+        this.props.onTryAutoSignup();
     }
 
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" exact component={BurgerBuilder} />
+                {/* for any unknown or wrong routes */}
+                <Redirect to="/" />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
+                    <Route path="/logout" component={Logout} />
+                    {/* for any unknown or wrong routes */}
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+
         return (
             <div>
                 <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/orders" component={Orders} />
-                        <Route path="/auth" component={Auth} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/" exact component={BurgerBuilder} />
-                    </Switch>
+                    <Switch>{routes}</Switch>
                 </Layout>
             </div>
         );
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapStateToProps = (state) => {
     return {
-        onTryAutoSingup: () => dispatch(actions.authCheckState()),
+        isAuthenticated: state.auth.token !== null,
     };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- my-app/src/containers/Orders/Orders.js
+++ my-app/src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
     componentDidMount() {
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render() {
@@ -23,18 +23,18 @@ class Orders extends Component {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onFetchOrders: (token) => dispatch(actions.fetchOrders(token)),
-    };
-};
-
 const mapStateToProps = (state) => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId,
     };
 };
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(userId, token)),
+    };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(withErrroHandler(Orders, axios));
